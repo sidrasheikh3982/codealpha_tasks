2980749bafa7a9bf79365063bd2934220778f39e
sidra sheikh <sidrasheikh3982@gmail.com>
2026-07-21 01:34:04 +0500

    Task 3 calculator

--- script.js
+++ script.js
@@ -1,359 +1,123 @@
-let images = [
 
-{
-pic:"https://images.unsplash.com/photo-1426604966848-d7adac402bff?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-title:"Wild Mountains",
-category:"nature"
-},
-
-{
-pic:"https://plus.unsplash.com/premium_photo-1720524513100-2fa5f656272f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
-title:"Golden Sunset",
-category:"nature"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1539634262233-7c0b48ab9503?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTF8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
-title:"Peaceful Forest",
-category:"nature"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
-title:"River Reflections",
-category:"nature"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
-title:"Autumn Beauty",
-category:"nature"
-},
-
-{
-pic:"https://plus.unsplash.com/premium_photo-1710030733154-16b30a0f944f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
-title:"Nature Escape",
-category:"nature"
-},
-
-{
-pic:"https://plus.unsplash.com/premium_photo-1672116452571-896980a801c8?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D",
-title:"Modern Skyline",
-category:"city"
-},
-
-{
-pic:"https://plus.unsplash.com/premium_photo-1682656220562-32fde8256295?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8fDA%3D",
-title:"City Lights",
-category:"city"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1464938050520-ef2270bb8ce8?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGNpdHl8ZW58MHx8MHx8fDA%3D",
-title:"Urban Dreams",
-category:"city"
-},
-
-{
-pic:"https://media.istockphoto.com/id/2225405260/photo/london-cityscape-with-the-gherkin-building.webp?a=1&b=1&s=612x612&w=0&k=20&c=9ZzyyfUiXJHBG0wTkA2AePTcBuDpYvf8vJuDgpcymPc=",
-title:"London View",
-category:"city"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1517511620798-cec17d428bc0?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGNpdHl8ZW58MHx8MHx8fDA%3D",
-title:"Downtown Streets",
-category:"city"
-},
-
-{
-pic:"https://plus.unsplash.com/premium_photo-1680667682187-52fd5e203efb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fGNpdHl8ZW58MHx8MHx8fDA%3D",
-title:"Night Vibes",
-category:"city"
-},
-{
-pic:"https://images.unsplash.com/photo-1513407030348-c983a97b98d8?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzh8fGNpdHl8ZW58MHx8MHx8fDA%3D",
-title:"City Architecture",
-category:"city"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1499092346589-b9b6be3e94b2?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODR8fGNpdHl8ZW58MHx8MHx8fDA%3D",
-title:"Skyline Sunset",
-category:"city"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-title:"Best Friends",
-category:"friends"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1444210971048-6130cf0c46cf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGZyaWVuZHN8ZW58MHx8MHx8fDA%3D",
-title:"Forever Together",
-category:"friends"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1536010305525-f7aa0834e2c7?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZyaWVuZHN8ZW58MHx8MHx8fDA%3D",
-title:"Happy Moments",
-category:"friends"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1503431153573-96e959f4d9b7?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTV8fGZhbWlseXxlbnwwfHwwfHx8MA%3D%3D",
-title:"Family Love",
-category:"family"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1622610607501-32ac9c927216?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGZhbWlseXxlbnwwfHwwfHx8MA%3D%3D",
-title:"Sweet Home",
-category:"family"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1588979355313-6711a095465f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZmFtaWx5fGVufDB8fDB8fHww",
-title:"Family Time",
-category:"family"
-},
-
-{
-pic:"https://plus.unsplash.com/premium_photo-1661475916373-5aaaeb4a5393?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFtaWx5fGVufDB8fDB8fHww",
-title:"Precious Memories",
-category:"family"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1609147073438-577eda06bd60?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y291c2lufGVufDB8fDB8fHww",
-title:"Cousin Squad",
-category:"cousin"
-},
-
-{
-pic:"https://images.unsplash.com/photo-1627203030417-33b1129033d0?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvdXNpbnxlbnwwfHwwfHx8MA%3D%3D",
-title:"Fun Together",
-category:"cousin"
-},
-
-{
-pic:"https://media.istockphoto.com/id/2248437499/photo/mom-helping-young-son-hit-the-pi%C3%B1ata.webp?a=1&b=1&s=612x612&w=0&k=20&c=vjFm0QknSWvqSTtT0i1iHwcw6R3953r2uIuikRSYJwY=",
-title:"Celebration Time",
-category:"cousin"
-}
-
-];
-
-let gallery = document.querySelector(".gallery");
-let lightbox = document.querySelector(".lightbox");
-let bigimg = document.querySelector(".big-img");
-
-let closeBtn = document.querySelector(".close");
-let prevBtn = document.querySelector(".prev");
-let nextBtn = document.querySelector(".next");
-
-let buttons = document.querySelectorAll(".buttons button");
-let loadBtn = document.querySelector("#loadBtn");
-
-let currentImages = images;
-let currentIndex = 0;
-
-let visible = 8;
+let display = document.querySelector(".display");
 
 
+let buttons = document.querySelectorAll(".button");
 
-function showImages(arr){
 
-gallery.innerHTML = "";
+let string = "";
 
-currentImages = arr;
 
-arr.slice(0,visible).forEach(function(image,index){
+buttons.forEach(function(button){
 
-let card = document.createElement("div");
-card.classList.add("card");
+    button.addEventListener("click", function(){
 
-card.innerHTML = `
+        let value = button.innerText;
 
-<img src="${image.pic}">
-<h3>${image.title || image.category}</h3>
+      
+        if(value == "="){
 
-`;
+            try{
 
-card.addEventListener("click",function(){
+                string = eval(string).toString();
+                display.value = string;
 
-currentIndex = index;
+            }
 
-bigimg.src = image.pic;
+            catch{
 
-lightbox.style.display="flex";
+                display.value = "Error";
+                string = "";
 
-});
+            }
 
-gallery.appendChild(card);
+        }
+
+
+        else if(value == "C"){
+
+            string = "";
+            display.value = "";
+
+        }
+
+        else if(value == "⌫"){
+
+            string = string.slice(0,-1);
+            display.value = string;
+
+        }
+
+       
+        else{
+
+            string += value;
+            display.value = string;
+
+        }
+
+    });
 
 });
 
 
+document.addEventListener("keydown", function(e){
 
-if(visible >= arr.length){
+    
+    if(e.key >= "0" && e.key <= "9"){
 
-loadBtn.style.display="none";
+        string += e.key;
+        display.value = string;
 
-}else{
+    }
 
-loadBtn.style.display="block";
+  
+    else if(
+        e.key == "+" ||
+        e.key == "-" ||
+        e.key == "*" ||
+        e.key == "/" ||
+        e.key == "." ||
+        e.key == "%"
+    ){
 
-}
+        string += e.key;
+        display.value = string;
 
-}
+    }
 
+   
+    else if(e.key == "Enter"){
 
+        try{
 
-showImages(images);
+            string = eval(string).toString();
+            display.value = string;
 
+        }
 
+        catch{
 
+            display.value = "Error";
+            string = "";
 
+        }
 
-function filterImages(category){
+    }
 
-visible = 8;
+    
+    else if(e.key == "Backspace"){
 
+        string = string.slice(0,-1);
+        display.value = string;
 
+    }
 
-buttons.forEach(btn=>btn.classList.remove("active"));
+    else if(e.key == "Escape"){
 
-event.target.classList.add("active");
+        string = "";
+        display.value = "";
 
-
-
-if(category==="all"){
-
-showImages(images);
-
-return;
-
-}
-
-
-
-let filtered = images.filter(function(image){
-
-return image.category===category;
-
-});
-
-
-
-showImages(filtered);
-
-}
-
-
-
-
-
-loadBtn.addEventListener("click",function(){
-
-visible +=4;
-
-showImages(currentImages);
+    }
 
 });
-
-
-
-
-
-nextBtn.addEventListener("click",function(){
-
-currentIndex++;
-
-if(currentIndex>=currentImages.length){
-
-currentIndex=0;
-
-}
-
-bigimg.src=currentImages[currentIndex].pic;
-
-});
-
-
-
-
-
-prevBtn.addEventListener("click",function(){
-
-currentIndex--;
-
-if(currentIndex<0){
-
-currentIndex=currentImages.length-1;
-
-}
-
-bigimg.src=currentImages[currentIndex].pic;
-
-});
-
-
-
-
-
-closeBtn.addEventListener("click",function(){
-
-lightbox.style.display="none";
-
-});
-
-
-
-
-
-lightbox.addEventListener("click",function(e){
-
-if(e.target===lightbox){
-
-lightbox.style.display="none";
-
-}
-
-});
-
-
-
-
-
-document.addEventListener("keydown",function(e){
-
-if(lightbox.style.display==="flex"){
-
-if(e.key==="ArrowRight"){
-
-nextBtn.click();
-
-}
-
-if(e.key==="ArrowLeft"){
-
-prevBtn.click();
-
-}
-
-if(e.key==="Escape"){
-
-lightbox.style.display="none";
-
-}
-
-}
-
-});
-
-
-
-
-
