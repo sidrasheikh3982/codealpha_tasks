837addcbd12de9a620322941d6e9215e1046b881
sidra sheikh <sidrasheikh3982@gmail.com>
2026-07-21 01:31:23 +0500

    Task 2 Image Gallery

--- script.js
+++ script.js
@@ -1,187 +1,359 @@
-let loader = document.querySelector("#loader");
-let main = document.querySelector("#main");
+let images = [
 
-setTimeout(function(){
+{
+pic:"https://images.unsplash.com/photo-1426604966848-d7adac402bff?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+title:"Wild Mountains",
+category:"nature"
+},
 
-    loader.style.display = "none";
-    main.style.display = "block";
+{
+pic:"https://plus.unsplash.com/premium_photo-1720524513100-2fa5f656272f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
+title:"Golden Sunset",
+category:"nature"
+},
 
-},2000);
-    let typed = new Typed("#element", {
-    strings: [
-        "Frontend Web Developer",
-        "Basic Graphic Designer",
-        "Agentic AI Enthusiast"
-    ],
-    typeSpeed: 80,
-    backSpeed: 50,
-    backDelay: 1000,
-    loop: true
+{
+pic:"https://images.unsplash.com/photo-1539634262233-7c0b48ab9503?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTF8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
+title:"Peaceful Forest",
+category:"nature"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
+title:"River Reflections",
+category:"nature"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
+title:"Autumn Beauty",
+category:"nature"
+},
+
+{
+pic:"https://plus.unsplash.com/premium_photo-1710030733154-16b30a0f944f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D",
+title:"Nature Escape",
+category:"nature"
+},
+
+{
+pic:"https://plus.unsplash.com/premium_photo-1672116452571-896980a801c8?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D",
+title:"Modern Skyline",
+category:"city"
+},
+
+{
+pic:"https://plus.unsplash.com/premium_photo-1682656220562-32fde8256295?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8fDA%3D",
+title:"City Lights",
+category:"city"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1464938050520-ef2270bb8ce8?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGNpdHl8ZW58MHx8MHx8fDA%3D",
+title:"Urban Dreams",
+category:"city"
+},
+
+{
+pic:"https://media.istockphoto.com/id/2225405260/photo/london-cityscape-with-the-gherkin-building.webp?a=1&b=1&s=612x612&w=0&k=20&c=9ZzyyfUiXJHBG0wTkA2AePTcBuDpYvf8vJuDgpcymPc=",
+title:"London View",
+category:"city"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1517511620798-cec17d428bc0?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGNpdHl8ZW58MHx8MHx8fDA%3D",
+title:"Downtown Streets",
+category:"city"
+},
+
+{
+pic:"https://plus.unsplash.com/premium_photo-1680667682187-52fd5e203efb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fGNpdHl8ZW58MHx8MHx8fDA%3D",
+title:"Night Vibes",
+category:"city"
+},
+{
+pic:"https://images.unsplash.com/photo-1513407030348-c983a97b98d8?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzh8fGNpdHl8ZW58MHx8MHx8fDA%3D",
+title:"City Architecture",
+category:"city"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1499092346589-b9b6be3e94b2?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODR8fGNpdHl8ZW58MHx8MHx8fDA%3D",
+title:"Skyline Sunset",
+category:"city"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+title:"Best Friends",
+category:"friends"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1444210971048-6130cf0c46cf?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGZyaWVuZHN8ZW58MHx8MHx8fDA%3D",
+title:"Forever Together",
+category:"friends"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1536010305525-f7aa0834e2c7?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZyaWVuZHN8ZW58MHx8MHx8fDA%3D",
+title:"Happy Moments",
+category:"friends"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1503431153573-96e959f4d9b7?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTV8fGZhbWlseXxlbnwwfHwwfHx8MA%3D%3D",
+title:"Family Love",
+category:"family"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1622610607501-32ac9c927216?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGZhbWlseXxlbnwwfHwwfHx8MA%3D%3D",
+title:"Sweet Home",
+category:"family"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1588979355313-6711a095465f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZmFtaWx5fGVufDB8fDB8fHww",
+title:"Family Time",
+category:"family"
+},
+
+{
+pic:"https://plus.unsplash.com/premium_photo-1661475916373-5aaaeb4a5393?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFtaWx5fGVufDB8fDB8fHww",
+title:"Precious Memories",
+category:"family"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1609147073438-577eda06bd60?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y291c2lufGVufDB8fDB8fHww",
+title:"Cousin Squad",
+category:"cousin"
+},
+
+{
+pic:"https://images.unsplash.com/photo-1627203030417-33b1129033d0?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvdXNpbnxlbnwwfHwwfHx8MA%3D%3D",
+title:"Fun Together",
+category:"cousin"
+},
+
+{
+pic:"https://media.istockphoto.com/id/2248437499/photo/mom-helping-young-son-hit-the-pi%C3%B1ata.webp?a=1&b=1&s=612x612&w=0&k=20&c=vjFm0QknSWvqSTtT0i1iHwcw6R3953r2uIuikRSYJwY=",
+title:"Celebration Time",
+category:"cousin"
+}
+
+];
+
+let gallery = document.querySelector(".gallery");
+let lightbox = document.querySelector(".lightbox");
+let bigimg = document.querySelector(".big-img");
+
+let closeBtn = document.querySelector(".close");
+let prevBtn = document.querySelector(".prev");
+let nextBtn = document.querySelector(".next");
+
+let buttons = document.querySelectorAll(".buttons button");
+let loadBtn = document.querySelector("#loadBtn");
+
+let currentImages = images;
+let currentIndex = 0;
+
+let visible = 8;
+
+
+
+function showImages(arr){
+
+gallery.innerHTML = "";
+
+currentImages = arr;
+
+arr.slice(0,visible).forEach(function(image,index){
+
+let card = document.createElement("div");
+card.classList.add("card");
+
+card.innerHTML = `
+
+<img src="${image.pic}">
+<h3>${image.title || image.category}</h3>
+
+`;
+
+card.addEventListener("click",function(){
+
+currentIndex = index;
+
+bigimg.src = image.pic;
+
+lightbox.style.display="flex";
+
+});
+
+gallery.appendChild(card);
+
 });
 
 
 
-let about = document.querySelector("#about");
+if(visible >= arr.length){
 
-let heading = document.querySelector("#about h1");
-let para = document.querySelector("#about #para");
+loadBtn.style.display="none";
 
-window.addEventListener("scroll", function () {
+}else{
 
-    let top = about.getBoundingClientRect().top;
+loadBtn.style.display="block";
 
-    if (top < window.innerHeight - 100) {
-        heading.classList.add("show");
+}
 
-        setTimeout(function () {
-            para.classList.add("show");
-        }, 300);
-    }
-
-});
-let h2 = document.querySelector("#about h2");
-let p = document.querySelector("#p");
-let resumebtn = document.querySelector(".resumebtn");
+}
 
 
-window.addEventListener("scroll", function(){
 
-    let top = about.getBoundingClientRect().top;
+showImages(images);
 
-    if(top < window.innerHeight - 100){
-        h2.classList.add("show");
 
-        setTimeout(function(){
-            p.classList.add("show");
-        },300);
 
-        setTimeout(function(){
-            resumebtn.classList.add("show");
-        },600);
 
-    }
 
-});
+function filterImages(category){
 
-let cards = document.querySelectorAll(".skill-card");
-let skills = document.querySelector("#skills");
+visible = 8;
 
-window.addEventListener("scroll",function(){
 
-    let top = skills.getBoundingClientRect().top;
 
-    if(top < window.innerHeight-100){
+buttons.forEach(btn=>btn.classList.remove("active"));
 
-        cards.forEach(function(card,index){
+event.target.classList.add("active");
 
-            setTimeout(function(){
 
-                card.classList.add("show");
 
-            },index*150);
+if(category==="all"){
 
-        });
+showImages(images);
 
-    }
+return;
 
-});
-let projectHeading = document.querySelectorAll(".project-card h2");
-let projectPara = document.querySelectorAll(".project-card p");
+}
 
-window.addEventListener("scroll", function () {
 
-    projectHeading.forEach(function(heading, index){
 
-        let top = heading.getBoundingClientRect().top;
+let filtered = images.filter(function(image){
 
-        if(top < window.innerHeight - 100){
-
-            heading.classList.add("show");
-
-            setTimeout(function(){
-                projectPara[index].classList.add("show");
-            },300);
-
-        }
-
-    });
+return image.category===category;
 
 });
 
 
-let skillsHeading = document.querySelector("#skills h2");
-let skillsPara = document.querySelector("#skills p");
 
-window.addEventListener("scroll", function () {
+showImages(filtered);
 
-    let top = skills.getBoundingClientRect().top;
+}
 
-    if (top < window.innerHeight - 100) {
-        skillsHeading.classList.add("show");
 
-        setTimeout(function () {
-            skillsPara.classList.add("show");
-        }, 300);
-    }
 
-});
-let name = document.querySelector("#name");
-let form = document.querySelector("form");
-let email = document.querySelector("#email");
-let emailerror = document.querySelector("#emailerror");
 
-form.addEventListener("submit", function(dets){
-    dets.preventDefault();
 
-    if(name.value.length < 5){
-        document.querySelector("#nameerror").style.display = "block";
-    }
-    else{
-                document.querySelector("#nameerror").style.display = "none";
-    }
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    let emailans = emailRegex.test(email.value);
-    if(!emailans){
-        emailerror.style.display = "initial";
-        }
-        else{
-              emailerror.style.display = "none";
-        }
-});
-let project = document.querySelector("#project");
-let projectHead = document.querySelector("#project h1");
+loadBtn.addEventListener("click",function(){
 
-window.addEventListener("scroll", function () {
+visible +=4;
 
-    let top = project.getBoundingClientRect().top;
-
-    if (top < window.innerHeight - 100) {
-        projectHead.classList.add("show");
-    }
+showImages(currentImages);
 
 });
-let contact = document.querySelector("#contact");
 
-let contactHeading = document.querySelector("#contact h1");
-let contactPara = document.querySelector("#contact p");
 
-window.addEventListener("scroll", function () {
 
-    let top = contact.getBoundingClientRect().top;
 
-    if (top < window.innerHeight - 100) {
 
-        contactHeading.classList.add("show");
+nextBtn.addEventListener("click",function(){
 
-        setTimeout(function () {
-            contactPara.classList.add("show");
-        }, 300);
+currentIndex++;
 
-    }
+if(currentIndex>=currentImages.length){
+
+currentIndex=0;
+
+}
+
+bigimg.src=currentImages[currentIndex].pic;
 
 });
-let button = document.querySelector("#button");
 
-button.addEventListener("click", function () {
-    window.location.href = "#contact";
+
+
+
+
+prevBtn.addEventListener("click",function(){
+
+currentIndex--;
+
+if(currentIndex<0){
+
+currentIndex=currentImages.length-1;
+
+}
+
+bigimg.src=currentImages[currentIndex].pic;
+
 });
+
+
+
+
+
+closeBtn.addEventListener("click",function(){
+
+lightbox.style.display="none";
+
+});
+
+
+
+
+
+lightbox.addEventListener("click",function(e){
+
+if(e.target===lightbox){
+
+lightbox.style.display="none";
+
+}
+
+});
+
+
+
+
+
+document.addEventListener("keydown",function(e){
+
+if(lightbox.style.display==="flex"){
+
+if(e.key==="ArrowRight"){
+
+nextBtn.click();
+
+}
+
+if(e.key==="ArrowLeft"){
+
+prevBtn.click();
+
+}
+
+if(e.key==="Escape"){
+
+lightbox.style.display="none";
+
+}
+
+}
+
+});
+
+
+
+
+
